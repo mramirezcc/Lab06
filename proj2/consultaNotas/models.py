55b2de937abe763662f8a09920f625b5573bf8c5
mramirezcc <mramirezcc@unsa.edu.pe>
2024-05-30 00:54:43 -0500

    Creación de las clases necesarias en models.py

--- proj2/consultaNotas/models.py
+++ proj2/consultaNotas/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+
+    class Anio(models.IntegerChoices):
+        PRIMERO = 1
+        SEGUNDO = 2
+        TERCERO = 3
+        CUARTO = 4
+        QUINTO = 5
+
+    #Dado que un CUI empieza con el año de ingreso, no será
+    #necesario considerar ceros a la izquierda
+    cui = models.IntegerField(max_digits=8)
+    nombre = models.CharField(max_length=100)
+    anio = models.SmallIntegerField(choices=Anio.choices)
+
+    def __str__(self):
+        return self.nombre
+
+class Curso(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class NotasAlumnosPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_value=20, decimal_places=1)
+
+    def __str__(self):
+        return f"{self.alumno.nombre} tiene una nota de {self.nota} en {self.curso.codigo}"
