d71738845508abc186c8980889f78dee54895778
mramirezcc <mramirezcc@unsa.edu.pe>
2024-05-30 02:15:43 -0500

    Creación de html para la visualización de los datos... sería mejor en una tabla, se modificará posteriormente

--- proj2/consultaNotas/views.py
+++ proj2/consultaNotas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import AgregarNotasForm, NotasAlumnosPorCurso
 
 def agregar_nota(request):
-    if request.method == 'POST'
+    if request.method == 'POST':
         form = AgregarNotasForm(request.POST)
         if form.is_valid():
             form.save()
@@ -12,5 +12,5 @@ def agregar_nota(request):
         return render(request, 'consultaNotas/agregar_nota.html', {'form', form})
 
 def lista_notas(request):
-    notas = NotasAlumnosPorCurso(request):
+    notas = NotasAlumnosPorCurso(request)
     return render(request, 'consultaNotas/lista_notas.html', {'notas': notas})
